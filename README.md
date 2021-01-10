2139b96bd67a907c7372a054bf83c35329c58cac
Joel Chacón Castillo <joel.chacon@cimat.mx>
2021-01-10 14:46:29 -0600

    ..

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # wfg_problems
+Benchmark taken from "A Review of Multiobjective Test Problems and a Scalable Test Problem Toolkit" by Simon Huband + "minus" version taken from "Many-Objective Problesm Are Not Always Difficult for Pareto Dominance-Based Evolutionary Algorithms" by Hisao Ishibuchi et al.
--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+/*
+Benchmark taken from "A Review of Multiobjective Test Problems and a Scalable Test Problem Toolkit" by Simon Huband + "minus" version taken from "Many-Objective Problesm Are Not Always Difficult for Pareto Dominance-Based Evolutionary Algorithms" by Hisao Ishibuchi et al.
+*/
 #include <bits/stdc++.h>
 #include "Toolkit/ExampleProblems.h"
 #include "Toolkit/TransFunctions.h"
